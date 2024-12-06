9d361147852989d714e39f68f2d67c997d4ee0a6
D-H-O-R-A <diegoantunes2301@gmail.com>
2024-12-06 12:47:43 -0500

    Initial Commit

---
+++ CubeDetector/CubeDetectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CubeDetectorApp.swift
+//  CubeDetector
+//
+//  Created by user268572 on 12/6/24.
+//
+
+import SwiftUI
+
+@main
+struct CubeDetectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
